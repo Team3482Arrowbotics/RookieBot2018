9994cad83507c1cc4e50dabb9a5f8bc3a7938346
Team3482 <Team3482@DESKTOP-VTVRHSA>
2018-01-16 16:52:27 -0800

    Added everything but PID function.

--- RookieBot2018/src/org/usfirst/frc/team3482/robot/commands/AutoTurn.java
+++ RookieBot2018/src/org/usfirst/frc/team3482/robot/commands/AutoTurn.java
@@ -1,4 +1,26 @@
 package org.usfirst.frc.team3482.robot.commands;
 
+import org.usfirst.frc.team3482.robot.RobotMap;
+
 public class AutoTurn {
+	double currentAngle = RobotMap.navx.getAngle();
+	double angleGoal1 = currentAngle + 90;
+	double angleGoal2 = currentAngle - 90
+	public void turn(boolean x){
+		if (x = true){
+			while (true){
+				RobotMap.drive.arcadeDrive(90, 0);
+				if(currentAngle != angleGoal1){
+					// pid shit nibba
+				}
+			}
+		} else if (x = false){
+			while (true){
+				RobotMap.drive.arcadeDrive(-90, 0);
+				if (currentAngle != angleGoal2){
+					//pid shit nibba
+				}
+			}
+		}
+	}
 }
